f1eaa474731ede926e5b41fe6e2d2648205b7506
Dolesh kumar <hanubhai0007@gmail.com>
2026-09-12 18:42:12 +0530

    notification program05

---
+++ Program05.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASSIGNMENT_04
+{
+    internal class Program05
+    {
+        delegate void notification();
+        static void email()
+        {
+            Console.WriteLine("Email notification sent.");
+        }
+        static void sms()
+        {
+            Console.WriteLine("SMS notification sent.");
+        }
+        static void whatsapp()
+        {
+            Console.WriteLine("WhatsApp notification sent.");
+        }
+        public static void Main(string[] args)
+        {
+            notification obj = email;
+            obj += sms;
+            obj += whatsapp;
+            obj();
+        }
+    }
+}
