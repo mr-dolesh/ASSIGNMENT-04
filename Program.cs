af3e436be444f1f48bfdeba5a165eb2a7617ffc7
Dolesh kumar <hanubhai0007@gmail.com>
2026-09-12 17:40:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASSIGNMENT_04
+{
+    internal class Program
+    {
+        delegate void mydelegate(string message);
+        static void display(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            mydelegate obj = new mydelegate(display);
+            obj("Hello! This is delegate.");
+        }
+    }
+}
