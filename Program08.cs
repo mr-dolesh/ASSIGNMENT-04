c812e3f4b515a6c99bb048e610692f65fbdbe90b
Dolesh kumar <hanubhai0007@gmail.com>
2026-09-16 09:29:08 +0530

    the built-in Action delegate

---
+++ Program08.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ASSIGNMENT_04
+{
+    class Program08
+    {
+        static void MethodOne()
+        {
+            Console.WriteLine("Method One executed");
+        }
+        static void MethodTwo()
+        {
+            Console.WriteLine("Method Two executed");
+        }
+        static void MethodThree()
+        {
+            Console.WriteLine("Method Three executed");
+        }
+        static void Main(string[] args)
+        {
+            Action obj = MethodOne;
+
+            Console.WriteLine("Unicast Action:");
+            obj();
+
+            Action obj2 = MethodOne;
+            obj2 += MethodTwo;
+            obj2 += MethodThree;
+
+            Console.WriteLine("\nMulticast Action:");
+            obj2();
+        }
+
+    }
+}
