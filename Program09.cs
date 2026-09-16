fda6bdfcf061f0a7e30e571b30bcb49336ba17b8
Dolesh kumar <hanubhai0007@gmail.com>
2026-09-16 09:56:19 +0530

    the built-in Func delegate.

---
+++ Program09.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ASSIGNMENT_04
+{
+    internal class Program09
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        static void Main(string[] args)
+        {
+            Func<int, int, int> operation = Add;
+
+            Console.WriteLine("Unicast Func:");
+            int result = operation(10, 5);
+            Console.WriteLine("Add: " + result);
+
+            Func<int, int, int> operation2 = Add;
+
+            operation2 += Subtract;
+            operation2 += Multiply;
+
+            Console.WriteLine("\nMulticast Func:");
+
+            Delegate[] methods = operation2.GetInvocationList();
+            foreach (Delegate method in methods)
+            {
+                int returnValue = (int)method.DynamicInvoke(10, 5);
+                Console.WriteLine("Method Name: "+ method.Method.Name + ", Return Value: " + returnValue);
+            }
+        }
+    }
+}
