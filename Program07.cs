7db6e7598244fd3cb2961d271e313126c8388efc
Dolesh kumar <hanubhai0007@gmail.com>
2026-09-16 09:15:19 +0530

    Use GetInvocationList() to display the names...

---
+++ Program07.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program07
+{
+    delegate void MyDelegate();
+
+    static void MethodOne()
+    {
+        Console.WriteLine("Method One executed");
+    }
+
+    static void MethodTwo()
+    {
+        Console.WriteLine("Method Two executed");
+    }
+
+    static void MethodThree()
+    {
+        Console.WriteLine("Method Three executed");
+    }
+
+    public static void Main(string[] args)
+    {
+        MyDelegate d = MethodOne;
+        d += MethodTwo;
+        d += MethodThree;
+
+        Delegate[] methods = d.GetInvocationList();
+
+        Console.WriteLine("Methods stored in delegate:");
+
+        foreach (MyDelegate method in methods)
+        {
+            Console.WriteLine("Method Name: " + method.Method.Name);
+
+            method();
+        }
+    }
+}
